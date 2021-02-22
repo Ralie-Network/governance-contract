c783b8ba43f7ecd44677b16aae66a81805a2a25d
phillipe.menezes <phillipe.menezes@ieee.org>
2021-02-22 10:16:36 -0300

    added GovernanceReward deployment instructions

--- migrations/1_deploy_governance.js
+++ migrations/1_deploy_governance.js
@@ -1,7 +1,9 @@
-const GovernanceMothership = artifacts.require("GovernanceMothership");
+//const GovernanceMothership = artifacts.require("GovernanceMothership");
+const GovernanceRewards = artifacts.require("GovernanceRewards");
 
 module.exports = function (deployer) {
     const _token = '0x0317babef455a110054ae0aa3d4761d3a9a60523';
-
-  deployer.deploy(GovernanceMothership, _token);
+    const _motherboard = '0x3b4b537BCd98ec88730594F7C9d823a17b15E835';
+  //deployer.deploy(GovernanceMothership, _token);
+  deployer.deploy(GovernanceRewards, _token, _motherboard);
 };
